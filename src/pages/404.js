52e95e15e2c229b4c22711237df03114020a067a
Eric Pratt <ericpratt86@gmail.com>
2019-09-10 13:49:08 -0700

    404 styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,21 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import Banner from "../components/Banner"
+import styles from "../css/error.module.css"
 
 const error = () => {
-  return <Layout>Hello from 404</Layout>
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="oops it's a dead end">
+          <Link to="/" className="btn-white">
+            back to home page
+          </Link>
+        </Banner>
+      </header>
+    </Layout>
+  )
 }
 
 export default error
