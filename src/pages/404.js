352b3126265e308f4d8d7e87d9919e19fb412ad4
Eric Pratt <ericpratt86@gmail.com>
2019-09-12 12:48:27 -0700

    Added page transitions

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Layout from "../components/Layout"
 import Banner from "../components/Banner"
 import styles from "../css/error.module.css"
@@ -9,9 +9,9 @@ const error = () => {
     <Layout>
       <header className={styles.error}>
         <Banner title="oops it's a dead end">
-          <Link to="/" className="btn-white">
+          <AniLink fade to="/" className="btn-white">
             back to home page
-          </Link>
+          </AniLink>
         </Banner>
       </header>
     </Layout>
