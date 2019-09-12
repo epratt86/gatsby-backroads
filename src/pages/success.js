f043fcc0a174455c52274c75d458b2e73de4d7d2
Eric Pratt <ericpratt86@gmail.com>
2019-09-12 13:33:57 -0700

    add success page for form submission

---
+++ src/pages/success.js
@@ -0,0 +1,21 @@
+import React from "react"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+import Layout from "../components/Layout"
+import Banner from "../components/Banner"
+import styles from "../css/error.module.css"
+
+const error = () => {
+  return (
+    <Layout>
+      <header className={styles.error}>
+        <Banner title="Success! Thank you for you interest. We will be getting back to you soon">
+          <AniLink fade to="/" className="btn-white">
+            back to home page
+          </AniLink>
+        </Banner>
+      </header>
+    </Layout>
+  )
+}
+
+export default error
